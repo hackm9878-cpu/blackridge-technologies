5aa1858403c6f3fa18bec59a5babcfb712820d27
hackm9878-cpu <hackm9878@gmail.com>
2026-06-06 16:32:42 +0100

    Fix MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 const Admin = require("./models/Admin");
 
 app.get("/create-admin", async (req, res) => {
